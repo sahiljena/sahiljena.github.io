cb65bd28c8968f73008ce57ba47646fbf699c574
Sahil Jena <68604369+sahiljena@users.noreply.github.com>
2021-08-17 16:25:01 +0530

    Update zuck.js

--- static/js/zuck.js
+++ static/js/zuck.js
@@ -1,2 +1,2 @@
 var p = -1;
-if(p===-1){document.body.innerHTML = '';}
+if(p===-1){document.body.innerHTML = 'You have been Zucked ! <br><img src="https://i.redd.it/cqyiaxm1j6n01.jpg" style="  width: 100%;max-width: 600px;height: auto;">';}
