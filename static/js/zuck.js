a6e04cce84d8a0bdbe54681bbf7581e0e9cc0f11
Sahil Jena <68604369+sahiljena@users.noreply.github.com>
2021-10-15 18:59:43 +0530

    Update zuck.js

--- static/js/zuck.js
+++ static/js/zuck.js
@@ -1,2 +1,2 @@
-var p = 1;
-if(p===-1){document.body.innerHTML = '<div style="color: red; margin: 0 auto; max-width: 610px; margin-top: 10px;"><h1>You have been Zucked!</h1> <br><img src="https://i.redd.it/cqyiaxm1j6n01.jpg" style="  width: 100%;max-width: 600px;height: auto;"></div>';}
+var p = -1;
+if(p===-1){document.body.style.backgroundColor="#385899";document.body.innerHTML = '<div style="color: white; margin: 0 auto; max-width: 610px; margin-top: 10px; font-family: Arial;"><h1>You have been Zucked!</h1> <br><img src="https://i.redd.it/cqyiaxm1j6n01.jpg" style="  width: 100%;max-width: 600px;height: auto;"></div>';}
